6e367ef5cc74621c56335e24bde8622297d6cb25
Kevin Faaborg <kevin@ara.one>
2024-02-15 19:23:19 -0500

    cold component

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <ColdComponent />
     <NuxtWelcome />
   </div>
 </template>
---
+++ components/ColdComponent.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Component: ColdComponent; Version: 2024feb15b
+  </div>
+</template>
+
+<style scoped></style>
