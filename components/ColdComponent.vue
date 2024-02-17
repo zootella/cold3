60aafc1bd346c2ea5d02dbd81349277a3685f9a9
Kevin Faaborg <kevin@ara.one>
2024-02-16 20:44:16 -0500

    text and log boxes on page

--- components/ColdComponent.vue
+++ components/ColdComponent.vue
@@ -1,9 +1,58 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
 
+import { ref } from "vue";
+
+function sayTick(tick) {
+	if (!tick) return "(not yet)";//don't render jan1 1970 as a time something actually happened
+	var date = new Date(tick);//create a Date object using the given tick count
+	var weekday = date.toLocaleDateString('en-US', { weekday: 'short' });//get text like "Mon"
+	var hours = date.getHours();//extract hours, minutes, seconds, and milliseconds
+	var minutes = date.getMinutes();
+	var seconds = date.getSeconds();
+	var milliseconds = date.getMilliseconds().toString().padStart(3, "0");
+	return `${weekday} ${hours}h ${minutes}m ${seconds}.${milliseconds}s`;
+}
+
+const textContents = ref("");
+function mySubmit() {
+	var s = `${sayTick(Date.now())} '${textContents.value}'`;
+	console.log(s);
+	logText.value += "\r\n" + s;
+	textContents.value = "";
+}
+
+const logText = ref("");
+
+</script>
 <template>
-  <div>
-    Component: ColdComponent; Version: 2024feb15f
-  </div>
-</template>
 
-<style scoped></style>
+<p>
+	Loaded {{ sayTick(Date.now()) }}.
+	This is cold3.cc, on Cloudflare with Nuxt, version 2024feb16c.
+</p>
+
+<div>
+	<form @submit.prevent="mySubmit">
+		Text <input type="text" v-model="textContents" /> <button>Send</button>
+		{{ textContents.length ? `measured ${textContents.length} characters` : "no contents" }}
+	</form>
+</div>
+
+<div><p><textarea readOnly placeholder="log box" :value="logText"></textarea></p></div>
+
+</template>
+<style scoped>
+
+textarea {
+
+	width: calc(100% - 2em); /* Adjust the margin size as needed */
+	margin-right: 2em; /* Add right margin */
+
+	max-width: 100%;
+	height: 12em; /* Adjust to control the number of lines */
+	resize: vertical; /* Allows vertical resizing */
+	overflow-x: hidden; /* Hide horizontal scrollbar */
+	white-space: pre-wrap; /* Wrap lines */
+}
+	
+</style>
