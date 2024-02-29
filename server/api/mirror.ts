705a3081cb2477a3a5545f6c22a02fff5c6672a5
Kevin Faaborg <kevin@ara.one>
2024-02-29 13:54:20 -0500

    removed unused code

--- server/api/mirror.ts
+++ server/api/mirror.ts
@@ -1,17 +1,10 @@
 
-import { nanoid, customAlphabet } from "nanoid";
 import { unique } from "../../library/library.js";//on the server, can't use ~ and must use .js
 
 export default defineEventHandler((event) => {
 
-	//custom
-	const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	const length = 21;
-	let generator = customAlphabet(alphabet, length);
-	let id = generator();
-
 	let o = {};
-	o.message = "hello from cold3 api mirror, version 2024feb29e";
+	o.message = "hello from cold3 api mirror, version 2024feb29f";
 	o.serverTick = Date.now();
 	o.headers = event.req.headers;
 	o.secretLength = (process.env.MY_FIRST_SECRET) ? process.env.MY_FIRST_SECRET.length : 0;
