4211e494bdf4f37d6b524d6edcc0397ec273fa21
Kevin Faaborg <kevin@ara.one>
2024-05-17 22:18:58 -0400

    factored supabase into database.js

--- library/database.js
+++ library/database.js
@@ -7,6 +7,12 @@ interface between the application and the database
 keep it all here together for easy refactoring and auditing
 */
 
+import { toss } from './library0.js'
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(process.env.ACCESS_SUPABASE_URL, process.env.ACCESS_SUPABASE_KEY)
+
 
 /*
 SQL statements fed into Supabase to create the database tables.
@@ -25,6 +31,44 @@ CREATE TABLE table_counts (
 
 
 
+
+
+// Four functions for the row 'count_global' in table 'table_settings'
+
+// 1. Determine if the row exists
+export async function rowExists() {
+	// SQL equivalent: SELECT COUNT(key) FROM table_settings WHERE key = 'count_global'
+	let { data, error, count } = await supabase
+		.from('table_settings').select('key', { count: 'exact' }).eq('key', 'count_global')
+	if (error) throw error
+	return count > 0
+}
+
+// 2. Create the row with starting value zero
+export async function createRow() {
+	// SQL equivalent: INSERT INTO table_settings (key, value) VALUES ('count_global', '0')
+	let { data, error } = await supabase
+		.from('table_settings').insert([{ key: 'count_global', value: '0' }])
+	if (error) throw error
+}
+
+// 3. Read the value
+export async function readRow() {
+	// SQL equivalent: SELECT value FROM table_settings WHERE key = 'count_global'
+	let { data, error } = await supabase
+		.from('table_settings').select('value').eq('key', 'count_global')
+	if (error) throw error
+	return data[0]?.value
+}
+
+// 4. Write a new value
+export async function writeRow(newValue) {
+	// SQL equivalent: UPDATE table_settings SET value = 'newValue' WHERE key = 'count_global' RETURNING *
+	let { data, error } = await supabase
+		.from('table_settings').update({ value: newValue }).eq('key', 'count_global').select()
+	if (error) throw error
+	if (!data.length) throw new Error('no error from update, but also no updated rows')
+}
 
 
 
--- server/api/count.ts
+++ server/api/count.ts
@@ -1,8 +1,7 @@
 
 import { log, inspect } from '../../library/library0.js'
-import { createClient } from '@supabase/supabase-js';
+import { rowExists, createRow, readRow, writeRow } from '../../library/database.js'
 
-const supabase = createClient(process.env.ACCESS_SUPABASE_URL, process.env.ACCESS_SUPABASE_KEY)
 
 export default defineEventHandler(async (event) => {
 	let o = {}
@@ -56,46 +55,6 @@ export default defineEventHandler(async (event) => {
 	return o;
 });
 
-
-
-
-
-// Four functions for the row 'count_global' in table 'table_settings'
-
-// 1. Determine if the row exists
-async function rowExists() {
-	// SQL equivalent: SELECT COUNT(key) FROM table_settings WHERE key = 'count_global'
-	let { data, error, count } = await supabase
-		.from('table_settings').select('key', { count: 'exact' }).eq('key', 'count_global')
-	if (error) throw error
-	return count > 0
-}
-
-// 2. Create the row with starting value zero
-async function createRow() {
-	// SQL equivalent: INSERT INTO table_settings (key, value) VALUES ('count_global', '0')
-	let { data, error } = await supabase
-		.from('table_settings').insert([{ key: 'count_global', value: '0' }])
-	if (error) throw error
-}
-
-// 3. Read the value
-async function readRow() {
-	// SQL equivalent: SELECT value FROM table_settings WHERE key = 'count_global'
-	let { data, error } = await supabase
-		.from('table_settings').select('value').eq('key', 'count_global')
-	if (error) throw error
-	return data[0]?.value
-}
-
-// 4. Write a new value
-async function writeRow(newValue) {
-	// SQL equivalent: UPDATE table_settings SET value = 'newValue' WHERE key = 'count_global' RETURNING *
-	let { data, error } = await supabase
-		.from('table_settings').update({ value: newValue }).eq('key', 'count_global').select()
-	if (error) throw error
-	if (!data.length) throw new Error('no error from update, but also no updated rows')
-}
 
 
 
