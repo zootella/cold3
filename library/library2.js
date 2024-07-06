a43a8debc41345289f194ff8bd227bf4d88b9ecd
Kevin Faaborg <kevin@ara.one>
2024-07-06 18:25:56 -0400

    do not share your browser tag

--- library/library2.js
+++ library/library2.js
@@ -3,7 +3,8 @@
 //grow them here, then probably refactor them out into named files in this library folder
 //actually don't do this, it's library1.js and the named files
 
-import { test, ok, log, composeLog } from './library0.js'
+import { noop, test, ok, log, composeLog, Now, subtleHash, Data } from './library0.js'
+import { Tag } from './library1.js'
 
 
 
@@ -58,8 +59,165 @@ function logToLogflare(s) {
 
 
 
+
+
+
+
+//  _                                       _              
+// | |__  _ __ _____      _____  ___ _ __  | |_ __ _  __ _ 
+// | '_ \| '__/ _ \ \ /\ / / __|/ _ \ '__| | __/ _` |/ _` |
+// | |_) | | | (_) \ V  V /\__ \  __/ |    | || (_| | (_| |
+// |_.__/|_|  \___/ \_/\_/ |___/\___|_|     \__\__,_|\__, |
+//                                                   |___/ 
+
+function removeNumerals(s) {
+	return s.replace(/\d/g, '')
+}
 test(() => {
+	ok(removeNumerals('hello') == 'hello')
+	ok(removeNumerals('h3ll0') == 'hll')
 })
+
+function getBrowserFingerprint() {
+	let agent = removeNumerals(navigator.userAgent)//remove numerals so frequent automatic browser updates don't change the fingerprint
+
+	let renderer = '', vendor = ''
+	let c = document.createElement('canvas')
+	let g = c.getContext('webgl') || c.getContext('experimental-webgl')
+	if (g) {
+		let d = g.getExtension('WEBGL_debug_renderer_info')
+		if (d) {
+			renderer = g.getParameter(d.UNMASKED_RENDERER_WEBGL)
+			vendor = g.getParameter(d.UNMASKED_VENDOR_WEBGL)
+		}
+	}
+	//what about screen resolution? multiple desktop monitors, and the user turning their phone, make this not a good choice
+
+	return `agent:${agent};renderer:${renderer};vendor:${vendor};`
+}
+
+function getBrowserTag() {//create set and get if not found
+	const browserTagKey = 'current_session_password'
+	const browserTagValuePrefix = 'account_access_code_DO_NOT_SHARE_'//named these to discourage sharing, even if a n00b user is being coached by a hacker on reddit or discord to dig around the inspector
+
+	let value = localStorage.getItem(browserTagKey)
+	if (!value) {
+		value = browserTagValuePrefix + Tag()
+		localStorage.setItem(browserTagKey, value)
+	}
+	return value
+}
+
+function getBrowserFingerprintAndTag() {
+	return getBrowserFingerprint() + getBrowserTag()
+}
+
+async function timeBrowserHash() {
+	let t = Now()
+	let h = (await subtleHash(Data({text: getBrowserFingerprintAndTag()}))).base32()
+	log(`hashed to ${h} in ${Now() - t}ms`)//ok, this takes 8ms, unfortunately
+}
+
+test(async () => {
+	log(getBrowserFingerprintAndTag())
+	await timeBrowserHash()
+})
+/*
+examples:
+
+agent:Mozilla/. (Windows NT .; Win; x) AppleWebKit/. (KHTML, like Gecko) Chrome/... Safari/.;renderer:ANGLE (Intel, Intel(R) UHD Graphics 630 (0x00003E92) Direct3D11 vs_5_0 ps_5_0, D3D11);vendor:Google Inc. (Intel);account_access_code_DO_NOT_SHARE_hi1y5ICjnEQLVDKtawm0C
+
+SSFUSWZPQBDEVTOM5MERKXEXZSSHA7SZ6XJBECOKQ3CY3W6STS5A
+
+7ms
+
+concerns with fingerprinting and identifying the session:
+
+pinia, shouldn't browserTag live there?
+
+how long does this take? (7-10ms on your big windows desktop, measure on your phone)
+you'll include it in every single API call, as it identifies who the caller at least says they are
+you don't want it to slow down the very first page laod for the user
+you can cache it for the whole session--in a global? in pinia?
+you can't save it to local storage! or let pinia do this unknowingly!
+you need to only await for it on the very first API call, not at the start of the first page load
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+environment detection exploration
+a library function might be called by:
+-a lambda function running node, either simulated locally by Serverless Framework or deployed to AWS Lambda
+-a web worker, either simulated locally by wrangler, deployed to cloudflare workers running API code, or intended for client side but being hydrated by Nuxt on a worker
+-a web browser, like the user's real browser running on their device, with a page
+*/
+export function detectEnvironment() {
+	log(deindent(`
+		detect environment: (not sure if this works, test each place!)
+
+		'${typeof localStorage != 'undefined'}' localStorage ~ only browsers, and not hydration
+		'${typeof importScripts != 'undefined'}' importScripts ~ only web workers
+		'${typeof process != 'undefined'}' process ~ only node like lambda`))
+}
+noop(() => {
+	detectEnvironment()
+})
+/*
+didn't pick 'window' because it's there during nuxt's hydration step!
+
+still eluding that is detecting Nuxt's hydration step, though. concepts for doing that include:
+
+$nuxt or useNuxt or useNuxtApp or tryUseNuxtApp .context.ssrContext
+https://nuxt.com/docs/guide/going-further/internals
+all that seems deep under the hood
+
+process.client, which you found from earlier, and looks to be a Nuxt thing
+
+nuxt's <ClientOnly /> tag:
+https://nuxt.com/docs/api/components/client-only
+
+*/
+
+
+
+export function deindent(s) {
+	return s
+}
+/*
+and now you remember you'd like deindent
+
+maybe it does this:
+-deal with incoming \r\n and just \n
+-removes first line if blank
+-replaces all tabs with two spaces
+-counts the number of spaces leading the new first line
+-removes that number of spaces from all the remaining lines
+-adds a newline at the end
+
+[]and look for where, you think, you started writing this already
+
+*/
+
+
 
 
 
