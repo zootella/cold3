2f75f2a8ad36d33fe0a49cb647f4435490086415
Kevin Faaborg <kevin@ara.one>
2024-07-09 13:49:39 -0400

    version notes and esnext

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: 'cloudflare-pages',
-
-    //added to solve error on npm run build about es2019 not including bigint literals
-    esbuild: { options: { target: 'es2020' } }
-  },
-  devtools: { enabled: true }
+	nitro: {
+		preset: 'cloudflare-pages',
+		esbuild: { options: { target: 'esnext' } }//added to solve error on $ npm run build about es2019 not including bigint literals
+	},
+	devtools: { enabled: true }
 })
