3504cfbbe6ce02f5fdbc746e794491ce376a8d33
Kevin Faaborg <kevin@ara.one>
2024-07-14 22:15:03 -0400

    before packing lambdas

--- net23/src/handlers/hello1.js
+++ net23/src/handlers/hello1.js
@@ -3,7 +3,7 @@ export const handler = async (event) => {
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
-			message: 'hello1, version 2024jul13f',
+			message: 'hello1, version 2024jul14a',
 		}),
 	}
 }
