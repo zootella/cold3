5b5f25fb44e48ae9e4507382eb166d2119563488
Kevin Faaborg <kevin@ara.one>
2024-07-22 15:20:08 -0400

    before rollup

--- net23/src/hello2.js
+++ net23/src/hello2.js
@@ -1,9 +1,38 @@
 
+import { nanoid } from 'nanoid'
+
 export const handler = async (event) => {
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
-			message: 'and hello from hello2!',
+			message: 'and hello from hello2!, v3',
+			tag: nanoid()
 		}),
 	}
 }
+
+
+
+
+
+
+
+/*
+import nanoid
+import Tag
+big int literals
+run tests
+
+npm run pack
+npm run local
+
+
+
+*/
+
+
+
+
+
+
+
