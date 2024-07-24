c626cf1fd3dd2c250d9d7a4736f74dd9a82d6a48
Kevin Faaborg <kevin@ara.one>
2024-07-24 01:20:52 -0400

    secrets everywhere

--- net23/src/hello2.js
+++ net23/src/hello2.js
@@ -1,7 +1,7 @@
 
 import { nanoid } from 'nanoid'
 
-import { runTests } from '../../library/library0.js'
+import { runTests, log } from '../../library/library0.js'
 import '../../library/library1.js'
 import '../../library/library2.js'
 import '../../library/database.js'
@@ -11,15 +11,18 @@ export const handler = async (event) => {
 	//try some big ints
 	let answer = BigInt(Date.now()) * 2n
 
-	console.log(typeof runTests)
+	//confirm we can get to environment variables and cloud secrets
+	let access = 'not found'
+	if (typeof process.env.ACCESS_NETWORK_23 == 'string') access = process.env.ACCESS_NETWORK_23.length
 
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
 			message: 'hello2, version 2024jul22i',
-      answer: `${answer}`,
+			answer: `${answer}`,
 			tag: nanoid(),
-			test: await runTests()
-		}),
+			test: await runTests(),
+			access: access
+		})
 	}
 }
