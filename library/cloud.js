3e9d902308dbcaa9efaed43657aca740763d11ac
Kevin Faaborg <kevin@ara.one>
2024-09-20 15:58:51 -0600

    before cleaning up cloud

--- library/cloud.js
+++ library/cloud.js
@@ -8,6 +8,211 @@ import { Tag, checkTag } from './library1.js'
 
 
 
+//TODO pretty sure you should get rid of this:
+/*
+*/
+//global references to use throughout this invocation we find ourselves in for this request
+let _event = null//the event or request object cloudflare workers or lambda hand us at the start of running for this request
+export function saveCloudEvent(e) { _event = e }//call at the start of both lambda and worker request handlers
+export function getCloudEvent() { return _event }//now you can get it when you need it, rather than passing it everywhere
+/*
+*/
+
+
+
+
+
+
+
+
+
+
+export async function sendAmazonEmail(c) {
+	let {fromName, fromEmail, toEmail, subjectText, bodyText, bodyHtml} = c
+	const ses = await loadAmazonEmail()
+	let q = {
+		Source: `${fromName} <${fromEmail}>`,//must be verified email or domain
+		Destination: { ToAddresses: [toEmail] },
+		Message: {
+			Subject: { Data: subjectText },
+			Body: {//both plain text and html for multipart/alternative email format
+				Text: { Data: bodyText },
+				Html: { Data: bodyHtml }
+			}
+		}
+	}
+	let result, error, success = true
+	q.tick = Now()
+
+	try {
+		result = await ses.sendEmail(q).promise()
+		//sanity check to set success false
+	} catch (e) { error = e; success = false }
+
+	let t = Now()
+	return {c, q, p: {success, result, error, tick: t, duration: t - q.tick}}
+}
+async function sendSendgridEmail1(c) {
+	let {fromName, fromEmail, toEmail, subjectText, bodyText, bodyHtml} = c
+	let q = {
+		resource: process.env.ACCESS_SENDGRID_URL,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': 'Bearer '+process.env.ACCESS_SENDGRID_KEY
+		},
+		body: JSON.stringify({
+			from: { name: fromName, email: fromEmail },
+			personalizations: [{ to: [{ email: toEmail }] }],
+			subject: subjectText,
+			content: [
+				{ type: 'text/plain', value: bodyText },
+				{ type: 'text/html',  value: bodyHtml },
+			]
+		})
+	}
+	return await ashFetchum(c, q)
+}
+
+
+
+
+
+
+
+
+
+
+export async function sendAmazonText(c) {
+	let {toPhone, messageText} = c
+	const sns = await loadAmazonTexts()
+	let p = {
+		PhoneNumber: toPhone,//recipient phone number in E.164 format, libphonenumber-js can do this
+		Message: messageText,//must be 160 characters or less
+	}
+	let result, error, success = true
+	q.tick = Now()
+
+	try {
+		result = await sns.publish(p).promise()
+		//sanity check to set success false
+	} catch (e) { error = e; success = false }
+
+	let t = Now()
+	return {c, q, p: {success, result, error, tick: t, duration: t - q.tick}}
+}
+async function sendTwilioText(c) {
+	let {toPhone, messageText} = c
+	let q = {
+		resource: process.env.ACCESS_TWILIO_URL+'/Accounts/'+process.env.ACCESS_TWILIO_SID+'/Messages.json',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Authorization': 'Basic '+btoa(process.env.ACCESS_TWILIO_SID+':'+process.env.ACCESS_TWILIO_AUTH)
+		},
+		body: new URLSearchParams({
+			From: process.env.ACCESS_TWILIO_PHONE,//the phone number twilio rents to us to send texts from
+			To:   toPhone,//recipient phone number in E.164 format
+			Body: messageText
+		})
+	}
+	return await ashFetchum(c, q)//call my wrapped fetch
+}
+
+
+
+
+
+
+
+//older versions
+export async function dog(s)   { s = composeLog(s); await sendDatadogLog({s});  log('logged to datadog:',  s) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export async function logToFile(...a) {
+	let fs = await loadFs()
+	let s = composeLog(a)
+	await fs.appendFile('cloud.log', s+newline)
+}
+
+
+
+export async function actualDatadogLog(s) {
+	return await sendDatadogLog({s})
+}
+async function actualSendgridEmail(card, message) {
+
+}
+async function actualTwilioText(card, message) {
+
+}
+async function actualAmazonEmail(card, message) {
+
+}
+async function actualAmazonText(card, message) {
+
+}
+
+
+
+
+async function testEmailAmazon(card, message) {//live
+	let r = await sendEmailUsingAmazon(
+		card.fromName,
+		card.fromEmail,
+		card.toEmail1,
+		message,
+		'body text',
+		'<b>body html</b>')
+	log(look(r))
+}
+async function testEmailSendgrid(card, message) {//live
+	let result = await sendEmailUsingSendGrid(
+		card.fromName,
+		card.fromEmail,
+		card.toEmail1,
+		message,
+		'body text',
+		'<b>body html</b>')
+	log(look(result))
+}
+async function testTextAmazon(card, message) {//live
+	let result = await sendTextUsingAmazon(card.toPhone1, message)
+	log(look(result))
+}
+async function testTextTwilio(card, message) {//not approved yet
+	let result = await sendTextUsingTwilio(card.toPhone1, message)
+	log(look(result))
+}
+
+
+
+
+
+
 
 
 
--- library/cloud2.js
+++ library/cloud2.js
@@ -19,10 +19,6 @@ async function loadAmazon() {
 async function loadAmazonEmail() { if (!_ses) _ses = new (await loadAmazon()).SES(); return _ses }
 async function loadAmazonTexts() { if (!_sns) _sns = new (await loadAmazon()).SNS(); return _sns }
 
-//global references to use throughout this invocation we find ourselves in for this request
-let _event = null//the event or request object cloudflare workers or lambda hand us at the start of running for this request
-export function saveCloudEvent(e) { _event = e }//call at the start of both lambda and worker request handlers
-export function getCloudEvent() { return _event }//now you can get it when you need it, rather than passing it everywhere
 
 
 
@@ -34,118 +30,17 @@ export function getCloudEvent() { return _event }//now you can get it when you n
 
 
 
-export async function sendAmazonEmail(c) {
-	let {fromName, fromEmail, toEmail, subjectText, bodyText, bodyHtml} = c
-	const ses = await loadAmazonEmail()
-	let q = {
-		Source: `${fromName} <${fromEmail}>`,//must be verified email or domain
-		Destination: { ToAddresses: [toEmail] },
-		Message: {
-			Subject: { Data: subjectText },
-			Body: {//both plain text and html for multipart/alternative email format
-				Text: { Data: bodyText },
-				Html: { Data: bodyHtml }
-			}
-		}
-	}
-	let result, error, success = true
-	q.tick = Now()
-
-	try {
-		result = await ses.sendEmail(q).promise()
-		//sanity check to set success false
-	} catch (e) { error = e; success = false }
-
-	let t = Now()
-	return {c, q, p: {success, result, error, tick: t, duration: t - q.tick}}
-}
-async function sendSendgridEmail1(c) {
-	let {fromName, fromEmail, toEmail, subjectText, bodyText, bodyHtml} = c
-	let q = {
-		resource: process.env.ACCESS_SENDGRID_URL,
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization': 'Bearer '+process.env.ACCESS_SENDGRID_KEY
-		},
-		body: JSON.stringify({
-			from: { name: fromName, email: fromEmail },
-			personalizations: [{ to: [{ email: toEmail }] }],
-			subject: subjectText,
-			content: [
-				{ type: 'text/plain', value: bodyText },
-				{ type: 'text/html',  value: bodyHtml },
-			]
-		})
-	}
-	return await ashFetchum(c, q)
-}
-
-
-
-
-
-
-
-
-
-
-export async function sendAmazonText(c) {
-	let {toPhone, messageText} = c
-	const sns = await loadAmazonTexts()
-	let p = {
-		PhoneNumber: toPhone,//recipient phone number in E.164 format, libphonenumber-js can do this
-		Message: messageText,//must be 160 characters or less
-	}
-	let result, error, success = true
-	q.tick = Now()
-
-	try {
-		result = await sns.publish(p).promise()
-		//sanity check to set success false
-	} catch (e) { error = e; success = false }
-
-	let t = Now()
-	return {c, q, p: {success, result, error, tick: t, duration: t - q.tick}}
-}
-async function sendTwilioText(c) {
-	let {toPhone, messageText} = c
-	let q = {
-		resource: process.env.ACCESS_TWILIO_URL+'/Accounts/'+process.env.ACCESS_TWILIO_SID+'/Messages.json',
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'Authorization': 'Basic '+btoa(process.env.ACCESS_TWILIO_SID+':'+process.env.ACCESS_TWILIO_AUTH)
-		},
-		body: new URLSearchParams({
-			From: process.env.ACCESS_TWILIO_PHONE,//the phone number twilio rents to us to send texts from
-			To:   toPhone,//recipient phone number in E.164 format
-			Body: messageText
-		})
-	}
-	return await ashFetchum(c, q)//call my wrapped fetch
-}
-
-
-
-
-
-
-export async function snippet2() {
-
+//let's test this stuff with node on the command line
+export async function snippet(card) {
+	log('here is the snippet, where you will do logs, email, sms in node first and soon!')
+	log(look(card))
 
 }
 
 
 
 
-
-//older versions
-export async function dog(s)   { s = composeLog(s); await sendDatadogLog({s});  log('logged to datadog:',  s) }
-
-
-
-async function sendDatadogLog(c) {
+async function sendLog_useDatadog(c) {
 	let {s} = c
 	let q = {
 		resource: process.env.ACCESS_DATADOG_ENDPOINT,
@@ -162,10 +57,6 @@ async function sendDatadogLog(c) {
 	}
 	return await ashFetchum(c, q)
 }
-
-
-
-
 
 async function ashFetchum(c, q) {//takes c earlier called parameters and q an object of instructions to make the request
 	let o = {method: q.method, headers: q.headers, body: q.body}
@@ -197,76 +88,6 @@ async function ashFetchum(c, q) {//takes c earlier called parameters and q an ob
 
 
 
-//let's test this stuff with node on the command line
-export async function snippet(card) {
-
-}
-
-
-
-
-
-
-
-
-
-
-
-export async function logToFile(...a) {
-	let fs = await loadFs()
-	let s = composeLog(a)
-	await fs.appendFile('cloud.log', s+newline)
-}
-
-
-
-export async function actualDatadogLog(s) {
-	return await sendDatadogLog({s})
-}
-async function actualSendgridEmail(card, message) {
-
-}
-async function actualTwilioText(card, message) {
-
-}
-async function actualAmazonEmail(card, message) {
-
-}
-async function actualAmazonText(card, message) {
-
-}
-
-
-
-
-async function testEmailAmazon(card, message) {//live
-	let r = await sendEmailUsingAmazon(
-		card.fromName,
-		card.fromEmail,
-		card.toEmail1,
-		message,
-		'body text',
-		'<b>body html</b>')
-	log(look(r))
-}
-async function testEmailSendgrid(card, message) {//live
-	let result = await sendEmailUsingSendGrid(
-		card.fromName,
-		card.fromEmail,
-		card.toEmail1,
-		message,
-		'body text',
-		'<b>body html</b>')
-	log(look(result))
-}
-async function testTextAmazon(card, message) {//live
-	let result = await sendTextUsingAmazon(card.toPhone1, message)
-	log(look(result))
-}
-async function testTextTwilio(card, message) {//not approved yet
-	let result = await sendTextUsingTwilio(card.toPhone1, message)
-	log(look(result))
-}
 
 
 
