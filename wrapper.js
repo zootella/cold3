286d7fafdb49c07ad2622e5f557abc33e7d9126a
Kevin Faaborg <kevin@ara.one>
2024-10-07 20:46:00 -0600

    first working deployment with new wrangler

--- wrapper.js
+++ wrapper.js
@@ -1,6 +1,6 @@
 export const wrapper = {
 	name: 'cold3',
-	tick: 1728355211297,
+	tick: 1728355332513,
 	hash: 'YF7TDC6ZNYAOEK54J3RG5MHXJRVOWDD7LGVWG47ZBWATVCK7OMHA',
 	codeFiles: 109,
 	codeSize: 529588,
