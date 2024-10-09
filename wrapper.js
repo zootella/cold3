ce75c13ee3d5c2726acde90cc3fcf6589886b620
Kevin Faaborg <kevin@ara.one>
2024-10-09 13:17:28 -0600

    notes and code to make access encrypted

--- wrapper.js
+++ wrapper.js
@@ -1,9 +1,9 @@
 export const wrapper = {
 	name: 'cold3',
-	tick: 1728423196248,
-	hash: 'RECSA2LMI6IIN2P6HVURJX2SEVGEQMDSDWYK6MVIOLXYXZDHHUFA',
+	tick: 1728501432178,
+	hash: '34M7RPOCLZMSDCQW6KTXVGGIZWJF4DRPKMMH4ZGKS2SG3D4SCX3Q',
 	codeFiles: 107,
-	codeSize: 528197,
+	codeSize: 539002,
 	totalFiles: 112,
-	totalSize: 8386683
+	totalSize: 8397488
 }
