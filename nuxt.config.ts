f8d1c5fd8fa519a12cd6cf37a30291809cd7c8b3
Kevin Faaborg <kevin@ara.one>
2024-10-22 11:57:06 -0400

    before parcel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 
-//added for visualization
 import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
@@ -11,7 +10,39 @@ export default defineNuxtConfig({
 			options: {
 				target: 'esnext'//added to solve error on npm run build about es2019 not including bigint literals
 			}
+		},
+
+		//starting point to visualize nuxt server code, if you want to try that
+		/*
+		nitro: {
+			hooks: {
+				'compiled': async () => {
+					const { visualizer } = await import('rollup-plugin-visualizer')
+					const fs = await import('fs')
+					const path = './.output/server/server.mjs'// Path to Nitro's server-side output
+
+					const statsServer = visualizer({
+						filename: './stats-server-api.html',  // Server-side stats output file
+						template: 'treemap',                 // Visualization type
+						brotliSize: true,                    // Show brotli size
+					})
+
+					// Apply the plugin to the server bundle
+					const rollup = await import('rollup')
+					const bundle = await rollup.rollup({
+						input: path,
+						plugins: [statsServer],
+					})
+
+					// Write the bundle
+					await bundle.write({
+						file: path,
+						format: 'es'  // Output format for ESM
+					})
+				}
+			}
 		}
+		*/
 	},
 	modules: [
 		'nitro-cloudflare-dev'
@@ -22,10 +53,8 @@ export default defineNuxtConfig({
 	runtimeConfig: {//nuxt promises these will be available on the server side, and never exposed to a client
 		ACCESS_KEY_SECRET: process.env.ACCESS_KEY_SECRET
 	},
-
-	//added for visualization
 	build: {
-		sourcemap: true,
+		sourcemap: true,//added for visualizer
 	},
 	vite: {
 		plugins: [
