205fa9f3327ab28db0d75085e5d93807b09fae1d
Kevin Faaborg <zootella@zootella.com>
2024-11-02 13:39:25 -0400

    jimp and sharp create image

--- net23/persephone/persephone.js
+++ net23/persephone/persephone.js
@@ -12,17 +12,15 @@ async function loadGrand() {
 
 async function requireModules() {
 	let grand = await loadGrand()
-	let { Sticker, getAccess, log, look, Size } = grand
+	let { Sticker, getAccess, log, look, Size, Data } = grand
 
 	let cut = 512
 	let o = {}
 	try {
 		o.intro = "now let's try some modules"
 
-		let access = await getAccess()
-
+/*
 		//amazon
-
 		const { SESClient, GetSendQuotaCommand } = require('@aws-sdk/client-ses')
 		const mailClient = new SESClient({region: 'us-east-1'})
 		o.amazonMail = look(mailClient.config).slice(0, cut)
@@ -32,31 +30,39 @@ async function requireModules() {
 		} catch (e2) {//permissions error deployed, but chat is explaining iam roles to define in serverless.yml
 			o.amazonMailQuotaError = e2.stack
 		}
-
 		const { SNSClient } = require('@aws-sdk/client-sns')
 		const textClient = new SNSClient({region: 'us-east-1'})
 		o.amazonText = look(textClient.config).slice(0, cut)
 
 		//twilio and sendgrid
-
 		const _twilio = require('twilio')
 		const _sendgrid = require('@sendgrid/mail')
-
 		o.twilioRequired = look(_twilio).slice(0, cut)
 		o.sendgridRequired = look(_sendgrid).slice(0, cut)
-
+		let access = await getAccess()
 		let twilioClient = _twilio(access.get('ACCESS_TWILIO_SID'), access.get('ACCESS_TWILIO_AUTH_SECRET'))
 		o.twilioClient = look(twilioClient).slice(0, cut)
 		_sendgrid.setApiKey(access.get('ACCESS_SENDGRID_KEY_SECRET'))
+*/
 
 		//jimp and sharp
 
 		const { Jimp } = await import('jimp')//matches documentation for jimp v1, careful as chat only knows about jimp v0
-		const image = new Jimp({width: 120, height: 120})
+		const image = new Jimp({width: 200, height: 120})
 		let b = await image.getBase64('image/png')
-		o.jimpBase64 = b
+		o.jimpPngBase64 = b
 
-		//here, i'd like to convert this image to PNG, and get the bytes of the png file as base64
+		const _sharp = require('sharp')
+		const b2 = await _sharp({
+			create: {
+				width: 200,
+				height: 120,
+				channels: 4,
+				background: {r: 255, g: 0, b: 0, alpha: 1}
+			}
+		}).png().toBuffer()//returns a Node Buffer, which is a subclass of Uint8Array
+		let d = Data({array: b2})
+		o.sharpPngBase64 = d.base64()
 
 		/*
 		o.jimpRequired = look(_jimp).slice(0, cut)
@@ -83,6 +89,8 @@ async function requireModules() {
 }
 
 module.exports = { loadGrand, requireModules }
+
+
 
 
 
