0d20a34ececfe857bb572a03619e5b3aa9088bd1
Kevin Faaborg <zootella@zootella.com>
2024-11-20 12:34:00 -0700

    tested stowaway build on mac

--- net23/stowaway.js
+++ net23/stowaway.js
@@ -4,16 +4,17 @@ import fs from 'fs-extra'
 /*
 the node module sharp, which can resize and watermark images, relies heavily on platform-specific native libraries
 yarn install on windows gets the windows ones; on mac, the mac ones
-to work deployed to lambda, we need the ones for linux
+to work deployed to lambda, we need the ones for linux on amazon's graviton chip
 
-copy sharp native libraries for amazon linux alongside the windows or mac ones in @img
+use this script to copy sharp native libraries for linux alongside the windows or mac ones in @img
+you got the linux libraries from the docker container
 this sneaks them into the serverless package zip
 yarn will find them and throw them back overboard, so manually run $ yarn stowaway after add or install!
 
 example:
 $ yarn install   ~ corrects modules for the computer we're coding on
-$ yarn stowaway  ~ adds amazon linux sharp binaries alongside ones yarn put there for mac or windows
-$ yarn build     ~ serverless package makes net23.zip, not realizing there are extra binaries in there!
+$ yarn stowaway  ~ adds linux sharp binaries alongside ones yarn put there for local development
+$ yarn build     ~ serverless package makes net23.zip, taking the ones for linux by name
 $ yarn deploy    ~ serverless deploy (importantly skipping package first)
 
 "scripts": {
@@ -22,7 +23,8 @@ $ yarn deploy    ~ serverless deploy (importantly skipping package first)
 	"justdeploy": "serverless deploy --force --package .serverless", ~ upload without making first
 },
 justdeploy is for when you've made the zip in the docker container, copied it out, and deploy that
-this is the slower but cleaner and better way to deploy to lambda, which doesn't involve the stowaway trick
+that alternative is cleaner but more cumbersome
+as a future third method, you could also configure a docker container with permissions to deploy
 */
 
 await fs.copy('../../stowaway', 'node_modules/@img', {overwrite: false})
