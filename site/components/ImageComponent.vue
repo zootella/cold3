2862331ac117b03ba85e7899af44ec14ebe5d8f3
Kevin Faaborg <zootella@zootella.com>
2024-12-11 16:40:59 -0500

    simpler vue

--- site/components/ImageComponent.vue
+++ site/components/ImageComponent.vue
@@ -5,19 +5,14 @@ import {
 log, look,
 } from 'icarus'
 
-let {data} = useFetch('/api/image', {method: 'POST'})
-let source = computed(() => data.value?.source || '')
-//^not sure what the best pattern is; here's another one!
-/*
-ttd december, wait a minute, you went round in chat and ended up with what's above
-but then you're seeing in ping4.js maybe it's just {{ data.source }}
-*/
+let {data, error} = useFetch('/api/image', {method: 'POST'})
 
 </script>
 <template>
 
 <div>
-<img :src="source" />
+<p><img :src="data.source" :alt="data.sticker" /></p>
+<p>signed by <i>{{ data.sticker }}</i></p>
 </div>
 
 </template>
--- site/server/api/image.js
+++ site/server/api/image.js
@@ -1,6 +1,6 @@
 
 import {
-log, look, checkTag, Time,
+Sticker, log, look, checkTag, Time,
 doorWorker,
 vhsSign,
 } from 'icarus'
@@ -11,8 +11,8 @@ export default defineEventHandler(async (workerEvent) => {
 async function doorProcessBelow(door) {
 	let o = {}
 
-	let s = await vhsSign('/', 2*Time.hour)//generate a query string that will allow access to everything at vhs.net23.cc for the next two hours
-	o.source = 'https://vhs.net23.cc/banner.png'+'?'+s
+	o.source = 'https://vhs.net23.cc/banner.png?'+(await vhsSign('/', 2*Time.hour))//generate a query string that will allow access to everything at vhs.net23.cc for the next two hours
+	o.sticker = Sticker().all//note who we are, signing this link
 
 	return o
 }
