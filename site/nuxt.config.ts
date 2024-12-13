dd35b45922b5240c010ad195338a51b75f0442e1
Kevin Faaborg <zootella@zootella.com>
2024-12-13 16:50:24 -0500

    commented nuxt config with tailwind

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -1,42 +1,60 @@
 
-//cold3
+import {visualizer} from 'rollup-plugin-visualizer'//from visualizer; $ yarn build to make stats.html
+import {vite as vidstack} from 'vidstack/plugins'//from vidstack
 
-import {visualizer} from 'rollup-plugin-visualizer'//visualizer
-import {vite as vidstack} from 'vidstack/plugins'//vidstack
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-	compatibilityDate: '2024-04-03',//nuxt
-	devtools: {enabled: true},//nuxt
+	compatibilityDate: '2024-04-03',//from nuxt
+	devtools: {enabled: true},//from nuxt
 	modules: [
-		'nitro-cloudflare-dev',//cloudflare
-		'@nuxtjs/tailwindcss',//tailwind
-		'nuxt-og-image',//ogimage
+		'nitro-cloudflare-dev',//from cloudflare
+		'@nuxtjs/tailwindcss',//from tailwind
+		'nuxt-og-image',//from ogimage
 	],
 	vue: {
 		compilerOptions: {
-			isCustomElement: (tag) => tag.startsWith('media-'),//vidstack
+			isCustomElement: (tag) => tag.startsWith('media-'),//from vidstack
 		},
 	},
 	vite: {
 		plugins: [
-			vidstack(),//vidstack
-			visualizer({filename: './stats.html', template: 'treemap', brotliSize: true}),//visualizer
-		]
+			visualizer({//from visualizer
+				filename: './stats.html',
+				template: 'treemap',//try out "sunburst", "treemap", "network", "raw-data", or "list"
+				brotliSize: true
+			}),
+			vidstack(),//from vidstack
+		],
 	},
 	nitro: {
-		preset: 'cloudflare-pages',//cloudflare
-		esbuild: {options: {target: 'esnext'}},//added for bigint
+		preset: 'cloudflare-pages',//from cloudflare
+		esbuild: {
+			options: {
+				target: 'esnext',//added to solve error on npm run build about es2019 not including bigint literals
+			},
+		},
 	},
 
-	runtimeConfig: {ACCESS_KEY_SECRET: process.env.ACCESS_KEY_SECRET},//added for secrets
+	runtimeConfig: {//added for getAccess; nuxt promises these will be available on the server side, and never exposed to a client
+		ACCESS_KEY_SECRET: process.env.ACCESS_KEY_SECRET,
+	},
 
-	build: {sourcemap: true},//visualizer
+	build: {
+		sourcemap: true,//from visualizer; causes rollup to make stats.html
+	},
 
-	site: {url: 'https://cold3.cc', name: 'cold3.cc'},//ogimage
+	site: {//from ogimage
+		url: 'https://cold3.cc',//ogimage needs site's deployed domain to set absolute urls in the cards
+		name: 'cold3.cc',
+	},
 	ogImage: {
-		defaults: {cacheMaxAgeSeconds: 20*60},
-		runtimeCacheStorage: {driver: 'cloudflare-kv-binding', binding: 'OG_IMAGE_CACHE'}
+		defaults: {
+			cacheMaxAgeSeconds: 20*60,//20 minutes in seconds; default if you omit this is 3 days
+		},
+		runtimeCacheStorage: {
+			driver: 'cloudflare-kv-binding',
+			binding: 'OG_IMAGE_CACHE',
+		},
 	},
-
 })
