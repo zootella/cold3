810824b28d7fefdf65cf407973c6bc643bde7361
Kevin Faaborg <zootella@zootella.com>
2025-02-03 13:28:09 -0500

    before pinia store

--- site/components/HitComponent.vue
+++ site/components/HitComponent.vue
@@ -6,51 +6,6 @@ getBrowserTag,
 } from 'icarus'
 import {ref, reactive, onMounted, watch} from 'vue'
 
-/*
-//not sure if we'll get refs this way, or if they'll come from useFetch or fetch$
-let refHits = ref(0)
-let refDuration = ref(0)
-*/
-
-/*
-onMounted(async () => {//doesn't run on server, even when hydrating
-	//not sure if we need to do anything in here, actually
-})
-
-function clickedHit() {
-	log('clicked hit')
-}
-*/
-
-/*
-we can use an existing endpoint at /api/hit
-we'll POST action: 'Get.' or 'Increment.'
-and expect a response body r with r.hits set to a number, which the component should show
-*/
-
-/*
-this component is a simple little hit counter
-i'd like it to hydrate on the server first, so that a client that can't even run javascript can get the current hit count
-
-all of this is nuxt 3 with vue's composition api, and modern full stack javascript
-*/
-
-/*
-here are notes for later (not now)
-
-pinia
-1[] remake your global counter, which goes between useFetch and the database
-2[] make your pinia counter, which persists between routes
-3[] understand the right way to merge those two
-
-perhaps a simple way to merge the two
-on first load, useFetch gets the count as part of hydration. you can confirm that getbot save or curl contains the count, without even js on teh client!
-then, the count is saved in pinia, clicks away and back again don't both the server. you can see that the last fetch time is zero
-clicks to the button to increment call the server with $fetch
-*/
-
-
-
 let {data, pending, error} = useFetch(
 	'/api/hit',
 	{
@@ -61,11 +16,7 @@ let {data, pending, error} = useFetch(
 	}
 )
 
-//<p>{{refHits}} hits, fetch took {{refDuration}}ms <button class="pushy" @onClick="clickedHit">Hit</button></p>
-
-
 </script>
-
 <template>
 <div>
 
@@ -73,20 +24,7 @@ let {data, pending, error} = useFetch(
 
 <div v-if="pending">Loading hit count...</div>
 <div v-else-if="error">Error: {{ error.message }}</div>
-{{data?.hits}} hits from {{data?.sticker}}
+<div v-else>{{data.hits}} hits from {{data.sticker}}</div>
 
 </div>
 </template>
-
-
-
-
-
-
-
-
-
-
-
-
-
