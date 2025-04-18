d3d43b287740f1d3acd87adf782c31828b42c706
Kevin Faaborg <zootella@zootella.com>
2025-04-18 15:15:28 -0400

    just two

--- site/plugins/errorPlugin.js
+++ site/plugins/errorPlugin.js
@@ -6,13 +6,13 @@ siteError,
 
 export default defineNuxtPlugin((nuxtApp) => {
 
-	//2 of 3: Nuxt error hook: Catches failures during SSR, plugin initialization, and the very first hydrate/mount on the client
+	//Gets failures during SSR, plugin initialization, and the very first hydrate/mount on the client
 	nuxtApp.hook('app:error', async (error) => {//runs on server and client
 		await siteError({source: 'Nuxt.', error})
 		//return nothing, and let Nuxt carry on to show error.vue
 	})
 
-	//3 of 3: Nuxt hook for Vue errors: Catches errors within render functions, lifecycle hooks, setup, watchers, event handlers, and so on
+	//Gets errors within render functions, lifecycle hooks, setup, watchers, event handlers, and so on
 	nuxtApp.hook('vue:error', async (error, instance, info) => {//runs on server and client
 		await siteError({source: 'Vue.', error, instance, info})
 		//no rethrow or return, Nuxt will render error.vue
