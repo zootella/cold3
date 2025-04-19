56c939113eb0763e4a857503adeed96b1e50a2f0
Kevin Faaborg <zootella@zootella.com>
2025-04-18 21:46:55 -0400

    small cleanups

--- site/error.vue
+++ site/error.vue
@@ -1,26 +1,7 @@
-<script setup>
-
-/*
-ok, im going to keep the tailwind styles, but need to center along with page-container
-
-also, the button is colliding with the h1?
-*/
-
-</script>
 <template>
 <div class="page-container text-center">
 
-<p>error.vue</p>
-
-<p>To keep things secure, 
-
-to keep thing secure
-something unexpected happened in the page
-click to report this to staff
-then, refresh to keep going
-
-
-An unexpected event in the code on the page meant that we </p>
+<p>There was an unexpected error in the page.</p>
 
 <NavigateButton to="/error2">Continue...</NavigateButton>
 
--- site/pages/error2.vue
+++ site/pages/error2.vue
@@ -6,6 +6,18 @@ definePageMeta({layout: 'feed-layout', note: 'on error2'})
 let currentError = useError()
 log('in error.vue, the current error from useError():', look(currentError))
 
+
+/*
+error.vue - nuxt's default error component, which we keep simple, and not automatic
+pages/error2.vue - a regular page, which can be fancy
+
+so the idea here is you use turnstile to POST complete error details to an error logging endpoint
+and then tell the user to refresh to start over
+
+note that the click from error.vue can't be automatic, as that could cause an infinite loop!
+
+*/
+
 </script>
 <template>
 
