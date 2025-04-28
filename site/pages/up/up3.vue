616ee3b811e8ef921aad0a377216edb2158e6b7f
Kevin Faaborg <zootella@zootella.com>
2025-04-28 17:56:41 -0400

    notes

--- site/pages/up/up3.vue
+++ site/pages/up/up3.vue
@@ -24,6 +24,23 @@ the new tests page, with
 [x]a refresh button on the page
 [x]dynamic loading of worker and lambda tests, so you don't have to wait to navigate in
 []unlike everywhere else, catch an error, and show it in a <pre>, be able to make a mess in a test anywhere in the stack, persephone, an api endpoint, and see what went wrong exactly on the page
+
+
+
+notes from early run:
+
+KEVHNPK CloudPageServer 0ms ✅ 672 assertions in 76 tests on Mon05:47p33.498s
+KEVHNPK CloudPageClient 18ms ✅ 674 assertions in 77 tests on Mon05:47p33.718s
+KEVHNPK CloudNuxtServer 0ms ✅ 674 assertions in 77 tests on Mon05:47p33.824s
+KEVHNPK CloudLambda 322ms ✅ 680 assertions in 81 tests on Mon05:47p34.821s Reload
+
+maybe show outer and inner times, as the lambda ran tests for 322ms, but the fourth line takes a full second to arrive, as evidenced by the on times on the right
+
+also, how is 0ms possible? it's not; measure the actual durations correctly for nuxt server render and api handler
+
+
+
+
 */
 
 function hardReload() { window.location.reload() }//same as user clicking the browser's Reload button
