63b0a3a59699bb5dda5fc6d7c73a205cd0c31075
Kevin Faaborg <zootella@zootella.com>
2025-05-01 10:13:28 -0400

    notes

--- site/components/snippet1/HitComponent.vue
+++ site/components/snippet1/HitComponent.vue
@@ -3,6 +3,18 @@
 
 const flexStore = useFlexStore()
 await flexStore.getHits()
+/*
+ttd april
+this whole store where it renders on server if on the page for a first GET, and on the client if clicked into later
+is academic, isn't something you're going to actually use
+but another idea here is:
+
+let p = flexStore.getHits()
+if (process.sever) await p
+
+also isn't this what useState() is about?
+current behavior here is good, except if you click into a route that has the hit component, the POST interrupts the navigation, which is bad
+*/
 
 async function clickedHit() {
 	await flexStore.incrementHits()
