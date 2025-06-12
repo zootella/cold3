eecec24faabf7b7d2448c7898dc96b77b3af9789
Kevin Faaborg <zootella@zootella.com>
2025-06-12 16:11:10 -0600

    wrangler 4 not working well

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -5,7 +5,7 @@ import {vite as vidstack} from 'vidstack/plugins'//from vidstack
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-	compatibilityDate: '2024-04-03',//from nuxt
+	compatibilityDate: '2025-05-15',//from nuxt
 	devtools: {enabled: true},//from nuxt
 	modules: [
 		'nitro-cloudflare-dev',//from cloudflare
@@ -29,7 +29,11 @@ export default defineNuxtConfig({
 		],
 	},
 	nitro: {
-		preset: 'cloudflare-pages',//from cloudflare
+		preset: 'cloudflare_module',//from cloudflare
+		cloudflare: {
+			deployConfig: true,
+			nodeCompat: true,
+		},
 		esbuild: {
 			options: {
 				target: 'esnext',//added to solve error on npm run build about es2019 not including bigint literals
