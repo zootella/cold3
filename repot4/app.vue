594dbc82dfe4c3fbb48687340c4f8a51566195c8
Kevin Faaborg <zootella@zootella.com>
2025-06-18 13:57:47 -0600

    no node compatibility for new cloudflare workers sites oauth and repot4

--- repot4/app.vue
+++ repot4/app.vue
@@ -1,10 +1,38 @@
-<script>//in the workspace "repot4", this is the file ./app.vue
+<script setup>//in the workspace "repot4", this is the file ./app.vue
+
+import {
+
+//autoicarus, temporary for Nuxt in repot4
+wrapper, Sticker, isLocal, isCloud,
+Now, Time, Size, Limit, newline,
+defined, toss, log, look,
+noop, test, ok,
+
+toBoolean, toTextOrBlank,
+checkInt, minInt,
+intToText, textToInt,
+hasText, checkText,
+hasTextOrBlank, checkTextOrBlank,
+makePlain, makeObject, makeText,
+
+Tag, hasTag, checkTag, checkTagOrBlank,
+checkHash,
+
+dog, logAudit, logAlert,
+awaitDog, awaitLogAudit, awaitLogAlert,
+
+canGetAccess, getAccess,
+doorWorker, doorLambda,
+Task, fetchWorker, fetchLambda, fetchProvider,
+
+} from 'icarus'
+
 </script>
 
 <template>
 	<div>
 		<p>
-			hello verson 2025jun13, 31
+			hello on repot4, {{Sticker().all}}
 		</p>
 		<NuxtRouteAnnouncer />
 		<NuxtWelcome />
