3acacece1c9a6dbe4051aa896d5b21a3127620bf
Kevin Faaborg <zootella@zootella.com>
2025-06-25 15:21:57 -0600

    JNO lean forward

--- site/server/middleware/accessMiddleware.js
+++ site/server/middleware/accessMiddleware.js
@@ -30,5 +30,5 @@ Task, fetchWorker, fetchLambda, fetchProvider,
 */
 
 export default defineEventHandler((workerEvent) => {//middleware lets us see the event object before Nuxt calls the specific handler
-//	accessKey(workerEvent?.context?.cloudflare?.env)//save the secret from the dashboard in a module variable for getAccess() to use
+	accessKey(workerEvent?.context?.cloudflare?.env)//save the secret from the dashboard in a module variable for getAccess() to use
 })
