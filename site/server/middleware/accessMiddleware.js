cc37c11a657b4272b47fdbb6acbcf928524f57fe
Kevin Faaborg <zootella@zootella.com>
2025-06-25 15:35:39 -0600

    EBH secrets from dashboard through event in middleware

--- site/server/middleware/accessMiddleware.js
+++ site/server/middleware/accessMiddleware.js
@@ -1,34 +1,5 @@
 //./server/middleware/accessMiddleware.js
 
-/*
-import {
-
-//manual icarus import block for nuxt middleware
-wrapper, Sticker, isLocal, isCloud,
-Now, Time, Size, Limit, newline,
-defined, toss, log, look,
-noop, test, ok,
-
-toBoolean, toTextOrBlank,
-checkInt, minInt,
-intToText, textToInt,
-hasText, checkText,
-hasTextOrBlank, checkTextOrBlank,
-makePlain, makeObject, makeText,
-
-Tag, hasTag, checkTag, checkTagOrBlank,
-checkHash,
-
-dog, logAudit, logAlert,
-awaitDog, awaitLogAudit, awaitLogAlert,
-
-accessKey, canGetAccess, getAccess,
-doorWorker, doorLambda,
-Task, fetchWorker, fetchLambda, fetchProvider,
-
-} from 'icarus'
-*/
-
 export default defineEventHandler((workerEvent) => {//middleware lets us see the event object before Nuxt calls the specific handler
 	accessKey(workerEvent?.context?.cloudflare?.env)//save the secret from the dashboard in a module variable for getAccess() to use
 })
--- site/server/middleware/cookieMiddleware.js
+++ site/server/middleware/cookieMiddleware.js
@@ -1,34 +1,5 @@
 //./server/middleware/cookieMiddleware.js
 
-/*
-import {
-
-//manual icarus import block for nuxt middleware
-wrapper, Sticker, isLocal, isCloud,
-Now, Time, Size, Limit, newline,
-defined, toss, log, look,
-noop, test, ok,
-
-toBoolean, toTextOrBlank,
-checkInt, minInt,
-intToText, textToInt,
-hasText, checkText,
-hasTextOrBlank, checkTextOrBlank,
-makePlain, makeObject, makeText,
-
-Tag, hasTag, checkTag, checkTagOrBlank,
-checkHash,
-
-dog, logAudit, logAlert,
-awaitDog, awaitLogAudit, awaitLogAlert,
-
-accessKey, canGetAccess, getAccess,
-doorWorker, doorLambda,
-Task, fetchWorker, fetchLambda, fetchProvider,
-
-} from 'icarus'
-*/
-
 //  _                                       _              
 // | |__  _ __ _____      _____  ___ _ __  | |_ __ _  __ _ 
 // | '_ \| '__/ _ \ \ /\ / / __|/ _ \ '__| | __/ _` |/ _` |
