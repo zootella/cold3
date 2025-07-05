0d5d100167b52aca61e88a0ed132d14fd93b20ea
Kevin Faaborg <zootella@zootella.com>
2025-07-05 16:08:45 -0600

    returned to the state this morning

--- icarus/vite.config.js
+++ icarus/vite.config.js
@@ -1,21 +1,22 @@
-
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
-
 		/*
 		hot reload works great without this; maybe even better, even
 		you kept it, however, so Ctrl+S restarts the log output on the page, rather than growing it
 		*/
 		{
 			name: 'force-full-reload',
-			handleHotUpdate({file, server}) {
+			handleHotUpdate({ file, server }) {
 				if (file.endsWith('.js')) {
-					server.ws.send({type: 'full-reload', path: '*'})
+					server.ws.send({
+						type: 'full-reload',
+						path: '*'
+					});
 					return []//prevent any other hot module replacement handling for this update
 				}
 			}
