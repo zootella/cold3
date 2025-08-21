9a0981e17ea2816f32582651564f1d9a5653e9b9
Kevin Faaborg <zootella@zootella.com>
2025-08-21 16:11:57 -0600

    WDO three qr methods

--- site/components/snippet1/QrComponent.vue
+++ site/components/snippet1/QrComponent.vue
@@ -4,16 +4,32 @@ import { ref } from 'vue'
 import QRCode from 'qrcode'
 
 const addressRef = ref('')//input, user pastes in URL to make a QR code from it
-const imageRef = ref('')//output, data URL for img src of the QR code image
 const errorRef = ref('')//output, or error information trying to generate it
+
+const method1 = ref('')//img src png
+const method2 = ref('')//raw svg tag delivered with v-html
+const method3 = ref('')//img src svg
 
 async function generate() {
 	errorRef.value = ''
-	imageRef.value = ''
+	method1.value = ''
+	method2.value = ''
+	method3.value = ''
 	let address = addressRef.value.trim()
 	if (address) {
 		try {
-			imageRef.value = await QRCode.toDataURL(address, {width: 256, margin: 2})
+			const width = 256//units of natural pixels of generated raster image
+			const margin = 2//units of QR code blocks!
+
+			method1.value = await QRCode.toDataURL(address, {width, margin})
+			method2.value = await QRCode.toString(address, {type: 'svg', width, margin})//width sets SVG's internal coordinate precision; vector graphics will scale sharply to any size
+			method3.value = `data:image/svg+xml;base64,${btoa(method2.value)}`
+
+			log(look({
+				method1: method1.value,
+				method2: method2.value,
+				method3: method3.value,
+			}))
 		} catch (e) {
 			errorRef.value = `Caught error ${e}`
 		}
@@ -33,9 +49,11 @@ async function generate() {
 
 	<div v-if="errorRef"><pre>{{errorRef}}</pre></div>
 
-	<div class="flex justify-center py-10">
-		<img v-if="imageRef" :src="imageRef" />
-	</div>
+	<div class="py-4"><p>method 1: img src PNG, {{method1.length}} characters:</p><img v-if="method1" :src="method1" /></div>
+	<div class="py-4"><p>method 2: raw SVG, {{method2.length}} characters:</p><div v-if="method2" v-html="method2"></div></div>
+	<div class="py-4"><p>method 3: img src SVG, {{method3.length}} characters:</p><img v-if="method3" :src="method3" /></div>
+
+	<!-- note that we're using v-html safely in method2 above, but it is considered potentially unsafe! -->
 </div>
 
 </div>
