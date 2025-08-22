052b1ca530e981cf3f47c4bdb4f6fdc6a4dd34aa
Kevin Faaborg <zootella@zootella.com>
2025-08-22 16:01:28 -0600

    before starting to clean up mess

--- site/components/snippet1/QrComponent.vue
+++ site/components/snippet1/QrComponent.vue
@@ -1,18 +1,50 @@
 <script setup>//./components/QrComponent.vue
 
-import {
-} from 'icarus'
+/*
+this is in a full stack js web app, using nuxt 3 and vue's composition api
+we're deploying to cloudflare web workers
+and want to choose isomorphic js modules that work in all three of these environments:
+- browser, where you can do things like canvas
+- node, where you can do things like buffer
+- web worker, which is the most restrictive of the three!
+additionally, we want to avoid configure and usage complexities around detecting environments--rather, we want to write clean simple durable js code that we have full confidence will run great, everywhere!
+
+ok, we're evaluating two popular qr code modules:
+https://www.npmjs.com/package/qrcode
+good:
+- most popular, millions of weekly installs
+bad:
+- line (A) below causes crash deployed; the web worker server render
+
+https://www.npmjs.com/package/qrcode-generator
+good:
+- very popular, hundreds of thousands of weekly installs
+- zero dependency design, yarn add and you get one thing
+bad:
+- commonjs still, author hasn't updated to esm
+
+so my first question is this: is there a difference between doing the import at A or B?
+what happens during the build? the server render? upon user input?
+(as written, B doesn't run until user input, but something got the module built into the client bundle--these sorts of considerations)
+let's be rigorous, detailed, specific and correct!
+*/
+
 //import QRCode from 'qrcode'//(A) an import up here breaks ssr in the web worker!
+import QRCodeGenerator from 'qrcode-generator'//(C) alternative module we're evaluating
 
 const addressRef = ref('')//input, user pastes in URL to make a QR code from it
 const errorRef = ref('')//output, or error information trying to generate it
 
-const method1 = ref('')//img src png
-const method2 = ref('')//raw svg tag delivered with v-html
-const method3 = ref('')//img src svg
+const method1 = ref('')//qrcode: img src png
+const method2 = ref('')//qrcode: raw svg tag delivered with v-html
+const method3 = ref('')//qrcode: img src svg
+const method4 = ref('')//qrcode-generator: img src svg
 
 async function generate() {
+	log(look(QRCodeGenerator))//look at the static import
+
   const QRCode = await import('qrcode')//(B) my expectation is moving it here will be no different!
+  //(D) alternatively, we could do a dynamic import of qrcode-generator
 
 	errorRef.value = ''
 	method1.value = ''
