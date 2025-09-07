ac2be3bcb3cd0e672417383fcd433c0472f9ed46
Kevin Faaborg <zootella@zootella.com>
2025-09-06 21:38:54 -0600

    4VI viem and wagmi in site and component

--- site/components/snippet1/WalletDemo.vue
+++ site/components/snippet1/WalletDemo.vue
@@ -22,6 +22,22 @@ typeof useAccount === 'function'
 )
 }
 
+/*
+hi claude, this is a vue component in a nuxt 3 site deploying to cloudflare workers
+the script up here can run in the user's browser tab, or, before that, be server rendered by the web worker environment
+as you can see, we've added and imported a toolchest of web3 ethereum modules
+
+my next step is to code a series of simple, but still correct, smoke tests to put these modules through their paces
+tasks we can implement include:
+1 the user clicks to connect metamask and sees their address on the page
+2 the user proves to the server that they control their address (we have a nuxt api endpoint we can POST to)
+3 the page shows the current ETH price and block number, even before a wallet is connected
+what other tasks like this should we write user stories to identify and scope?
+
+let's work on these one at a time, piece by piece, keeping things working
+which is the best starting point?
+*/
+
 </script>
 <template>
 <div class="border border-gray-300 p-2">
