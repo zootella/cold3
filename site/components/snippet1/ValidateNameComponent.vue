ac5c3a558d4924c9a5d4ca3d1fb7a454ba99316c
Kevin Faaborg <zootella@zootella.com>
2025-11-02 14:08:51 -0700

    EBH testing after validate rename

--- site/components/snippet1/ValidateNameComponent.vue
+++ site/components/snippet1/ValidateNameComponent.vue
@@ -4,26 +4,33 @@ import {
 validateName,
 } from 'icarus'
 
-const refName2Box = ref(''); const refStatus2 = ref('')
-const refName1Box = ref(''); const refStatus1 = ref('')
-const refStatus = ref('')
+const refStatus = ref(false)//overall status
+const refName0 = ref('')//output for form 0, normalized to reserve
+const refName1 = ref(''); const refBox1 = ref('')//output text and input box for form 1, canonical for route
+const refName2 = ref(''); const refBox2 = ref('')//output text and input box for form 2, pretty for pages and cards
 
-watch([refName2Box], () => {//the top box controls the lower one
-	let v = validateName(refName2Box.value, Limit.name)
-	log(look({input: refName2Box.value, v}))
+watch([refBox2], () => {//box 2 on top controls box 1
+	let v = validateName(refBox2.value, Limit.name)
+	refStatus.value = v.ok
 	if (v.f2ok) {
-		refStatus2.value = v.f2//set the status here
-		refName1Box.value = v.f1//and the contents of the box below
+		refName2.value = v.f2//set the status here
+		refBox1.value = v.f1//and the contents of the box below
 	} else {
-		refStatus2.value = `(not valid yet, keep typing...)`
+		refName0.value = ''
+		refName1.value = ''
+		refName2.value = ''
 	}
 })
-watch([refName1Box], () => {//which is also independently editable
-	let v = validateName(refName1Box.value, Limit.name)
+watch([refBox1], () => {//which is also independently editable
+	let v = validateName(refBox1.value, Limit.name)
+	refStatus.value = v.ok
 	if (v.ok) {
-		refStatus1.value = v.f1//set the status here
+		refName0.value = v.f0
+		refName1.value = v.f1//set the status here
 	} else {
-		refStatus1.value = `(not valid yet, keep typing...)`
+		refName0.value = ''
+		refName1.value = ''
+		refName2.value = ''
 	}
 })
 
@@ -33,12 +40,14 @@ watch([refName1Box], () => {//which is also independently editable
 <p class="text-xs text-gray-500 mb-2 text-right m-0 leading-none"><i>ValidateNameComponent</i></p>
 
 <p>Choose your user name:</p>
-<p><input :maxlength="Limit.name" v-model="refName2Box" placeholder="name for pages" class="w-96" /></p>
-<p>On pages and cards you'll be <i>"{{refStatus2}}"</i></p>
+<p><input :maxlength="Limit.name" v-model="refBox2" placeholder="name for pages" class="w-96" /></p>
+<p>Valid {{refStatus ? '✅ Yes' : '❌ No'}}</p>
+<p>On pages and cards you'll be <i>{{refName2}}</i></p>
 
 <p>And from that, here's what your name will be in links. You can edit this box directly, too.</p>
-<p><input :maxlength="Limit.name" v-model="refName1Box" placeholder="name for links" class="w-96" /></p>
-<p>That'll come through as <code>https://cold3.cc/<b>{{refStatus1}}</b></code></p>
+<p><input :maxlength="Limit.name" v-model="refBox1" placeholder="name for links" class="w-96" /></p>
+<p>Your profile will be at <code>https://cold3.cc/{{refName1}}</code></p>
+<p>We'll reserve <code>{{refName0}}</code> for you, also.</p>
 
 </div>
 </template>
