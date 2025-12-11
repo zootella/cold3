96b27ffa389ad5ac509b0dc4135a10da66991e7b
Kevin Faaborg <zootella@zootella.com>
2025-12-11 03:27:05 -0500

    EYS password box has show hide control from lucide.dev

--- site/components/small/PasswordBox.vue
+++ site/components/small/PasswordBox.vue
@@ -3,8 +3,6 @@
 defineProps(['modelValue', 'placeholder'])
 const emit = defineEmits(['update:modelValue', 'input', 'enter'])
 
-const refShowing = ref(false)
-
 function onInput(e) {
 	emit('update:modelValue', e.target.value)
 	emit('input', e)
@@ -12,9 +10,9 @@ function onInput(e) {
 function onKeyup(e) {
 	if (e.key == 'Enter') emit('enter', e)
 }
-function onToggle() {
-	refShowing.value = !refShowing.value
-}
+
+const refShowing = ref(false)
+function onToggle() { refShowing.value = !refShowing.value }
 
 </script>
 <template>
@@ -25,19 +23,19 @@ function onToggle() {
 		:value="modelValue"
 		:placeholder="placeholder"
 		:maxlength="Limit.input"
-		class="w-full pr-10"
+		class="w-full pr-12"
 		@input="onInput"
 		@keyup="onKeyup"
 	/>
 	<button
 		type="button"
 		tabindex="-1"
-		class="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+		class="absolute right-0 top-1/2 -translate-y-1/2 w-11 h-11 p-2 text-gray-500 hover:text-gray-700"
 		@click="onToggle"
 	>
-		<svg v-if="refShowing" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>
-		<svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-.722-3.25"/><path d="M2 8a10.645 10.645 0 0 0 20 0"/><path d="m20 15-1.726-2.05"/><path d="m4 15 1.726-2.05"/><path d="m9 18 .722-3.25"/></svg>
-		<!-- icons from lucide.dev -->
+		<svg v-if="refShowing" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>
+		<!-- icon of open eye above, closed eyelid with eyelashes below; icons from lucide.dev -->
+		<svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-.722-3.25"/><path d="M2 8a10.645 10.645 0 0 0 20 0"/><path d="m20 15-1.726-2.05"/><path d="m4 15 1.726-2.05"/><path d="m9 18 .722-3.25"/></svg>
 	</button>
 </div>
 
