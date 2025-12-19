4a6ec1e0f3f3bbb4b66f67ece98eb00db44d2600
Kevin Faaborg <zootella@zootella.com>
2025-12-18 18:45:57 -0700

    ZJB more cosmetic edits

--- net23/build.js
+++ net23/build.js
@@ -9,12 +9,9 @@ log, look,
 
 async function main() {//build a lean net23/dist/.serverless/net23.zip with the right native binaries for Lambda
 
-	log('Emptying...')//empty the dist folder
 	await fs.remove('dist')
-	await fs.ensureDir('dist')
-
-	log('Seeding...')//copy lambda source files, persephone library files, serverless.yml and .env
-	await fs.copy('.env',           'dist/.env')
+	await fs.ensureDir('dist')//empty the dist folder
+	await fs.copy('.env',           'dist/.env')//copy lambda source files, persephone library files, serverless.yml and .env
 	await fs.copy('serverless.yml', 'dist/serverless.yml')
 	await fs.copy('src',            'dist/src')
 	await fs.copy('persephone',     'dist/persephone')
@@ -22,19 +19,19 @@ async function main() {//build a lean net23/dist/.serverless/net23.zip with the 
 	//make a package.json for dist based on net23's, with icarus's dependencies merged in
 	let p1 = JSON.parse(await fs.readFile('package.json'))
 	let p2 = JSON.parse(await fs.readFile('../icarus/package.json'))
-	p1.dependencies = {//the dist/package.json dependencies: {} object
+	p1.dependencies = {//set the dist/package.json dependencies to have
 		...p1.dependencies,//all the net23 dependencies
 		...p2.dependencies}//and additionally the icarus dependencies
-	delete p1.dependencies.icarus//remove the icarus workspace dependency "*"; we've brought in icarus' dependencies and will copy its code soon below
+	delete p1.dependencies.icarus//but not the icarus workspace dependency "*"; we have icarus' dependencies and its code is coming
 	await fs.writeFile('dist/package.json', JSON.stringify(p1, null, '\t'))
 
-	log('npm 📜 installing...')//have npm use dist/package.json to build a new clean production only dist/node_modules
+	log('build.js is npm 📜 installing...')//have npm use dist/package.json to build a new clean production only dist/node_modules
 	execSync('npm install --omit=dev --os=linux --cpu=arm64 --libc=glibc', {//install for amazon linux on their graviton chip to get the right sharp native binaries
 		cwd: 'dist',//run this command in the dist subfolder
 		stdio: 'inherit',//show its command line output
 	})
 
-	log('Placing 🪽 icarus...')//now that we have node_modules populated, copy in the icarus .js files
+	//now that we have node_modules populated, copy in the icarus .js files
 	await fs.ensureDir('dist/node_modules/icarus')//as though there's a node module named "icarus"
 	let files = await fs.readdir('../icarus')
 	for (let file of files) {
@@ -46,7 +43,7 @@ async function main() {//build a lean net23/dist/.serverless/net23.zip with the 
 	let {fileList} = await nodeFileTrace(entryPoints, {base: 'dist'})//from vercel nft's results, pull out the list of necessary files
 	let necessary = [...fileList].filter(f => f.startsWith('node_modules/'))//lambda, persephone, icarus files above we've already got
 
-	log(`...which identifies ${necessary.length} necessary files. Taking...`)
+	log(`Taking necessary files...`)
 	await fs.rename('dist/node_modules', 'dist/node_modules_source')
 	for (let file of necessary) {
 		await fs.ensureDir(path.dirname('dist/'+file))
@@ -54,10 +51,9 @@ async function main() {//build a lean net23/dist/.serverless/net23.zip with the 
 	}
 	await fs.remove('dist/node_modules_source')
 
-	log('Marking wrapper.js cloud ☁️ true...')
 	let p = 'dist/node_modules/icarus/wrapper.js'
 	let c = await fs.readFile(p, 'utf8')
-	c = c.replace('"cloud": false', '"cloud": true')
+	c = c.replace('"cloud": false', '"cloud": true')//in wrapper.js, set cloud to true
 	await fs.writeFile(p, c)
 }
 
