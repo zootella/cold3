99ec8a888343d74d63ddced5cf0d9cc69eae6a49
Kevin Faaborg <zootella@zootella.com>
2025-12-20 22:49:02 -0700

    ACT another draft new Button

--- site/components/snippet1/OauthDemo.vue
+++ site/components/snippet1/OauthDemo.vue
@@ -6,14 +6,15 @@ originOauth,
 
 const origin = originOauth()//will be "https://oauth.cold3.cc" cloud, or "http://localhost:5173" local
 
-const refDoing = ref(false)
+const refClickedProvider = ref(null)
 
-const buttonState = computed(() => {
-	return refDoing.value ? 'ghost' : 'ready'//clicked button shows 'doing' via Button's internal state, others show 'ghost'
-})
+function buttonState(provider) {
+	if (!refClickedProvider.value) return 'ready'
+	return refClickedProvider.value == provider ? 'doing' : 'ghost'
+}//interestingly, this works with this function in the template, rather than needing to use a computed property, ttd december
 
 async function clicked(provider) {
-	refDoing.value = true//note we don't need to set false because href= is going to tear down the whole Nuxt application
+	refClickedProvider.value = provider//note we don't need to set null because href= is going to tear down the whole Nuxt application
 	let response = await fetchWorker('/api/oauth', {method: 'POST', body: {action: 'OauthStart.'}})
 	window.location.href = `${originOauth()}/continue/${provider}?envelope=${response.envelope}`//encoding? base62 don't need no stinkin' encoding 👒
 }
@@ -23,9 +24,9 @@ async function clicked(provider) {
 <div class="border border-gray-300 p-2">
 <p class="text-xs text-gray-500 mb-2 text-right m-0 leading-none"><i>OauthDemo</i></p>
 
-<div><Button :state="buttonState" :click="() => clicked('google')">Continue with Google</Button></div>
-<div><Button :state="buttonState" :click="() => clicked('twitter')">Continue with 𝕏</Button></div>
-<div><Button :state="buttonState" :click="() => clicked('discord')">Continue with Discord</Button> test flow here</div>
+<div><Button :state="buttonState('google')"  :click="() => clicked('google') ">Continue with Google</Button></div>
+<div><Button :state="buttonState('twitter')" :click="() => clicked('twitter')">Continue with 𝕏</Button></div>
+<div><Button :state="buttonState('discord')" :click="() => clicked('discord')">Continue with Discord</Button> test flow here</div>
 
 </div>
 </template>
