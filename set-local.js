f4f7d76b3dd7cecbe082a5ce8b9da7981163f9b7
Kevin Faaborg <zootella@zootella.com>
2026-01-07 20:46:39 -0700

    S45 inlined deploy before and after using direct node calls

--- set-local.js
+++ set-local.js
@@ -3,7 +3,7 @@ import {log, look} from 'icarus'
 import {promises as fs} from 'fs'
 
 async function main() {
-	let p = 'icarus/wrapper.js'
+	let p = '../icarus/wrapper.js'
 	let c = await fs.readFile(p, 'utf8')
 	c = c.replace('"cloud": true', '"cloud": false')
 	await fs.writeFile(p, c)
