2fdf4e2cd82a05550aac413fd0c12b05281946a7
Kevin Faaborg <zootella@zootella.com>
2026-01-27 14:39:53 -0700

    5GU upload endpoint stub

---
+++ site/server/api/upload.js
@@ -0,0 +1,7 @@
+
+/*
+hi claude, ok so as we continue along teh plan in media.md, my guess is that the nuxt page will need to talk to the nuxt worker
+to begin, for instance, to get a valid envelope to use as permission when the page talks directly to the new /upload lambda, for instance
+if that's the case, here's the file for that endpoint
+when making a new endpoint, please of course follow the patterns and use the helpers of other endpoints you see nearby
+*/
