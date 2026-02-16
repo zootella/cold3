1d69a5c93f7315c0946e06949ea213059f1a58f1
Kevin Faaborg <zootella@zootella.com>
2026-02-16 11:57:38 -0700

    C6R pageshow for thawing

--- site/app/components/snippet1/OauthDemo.vue
+++ site/app/components/snippet1/OauthDemo.vue
@@ -19,9 +19,19 @@ async function clicked(provider) {
 	window.location.href = `${originOauth()}/continue/${provider}?envelope=${task.envelope}`//encoding? base62 don't need no stinkin' encoding 👒
 }
 
+onMounted(() => { window.addEventListener('pageshow', onShow) })//listen for pageshow to be able to tell if the user clicks Back here
+onUnmounted(() => { window.removeEventListener('pageshow', onShow) })
+function onShow(event) {
+	if (event.persisted) {//true if the browser just thawed the page state from bfcache, meaning the user clicked Back from one of the oauth permission pages like Google or Discord
+		refClickedProvider.value = null//reset our memory of which one they clicked so they can choose again
+		refKey.value++//neat Vue trick to get a single dom element to render anew
+	}
+}
+const refKey = ref(0)//increment to force Vue to recreate
+
 </script>
 <template>
-<div class="border border-gray-300 p-2">
+<div class="border border-gray-300 p-2" :key="refKey"><!-- changing key makes Vue destroy and recreate this div and all child components, resetting their internal state -->
 <p class="text-xs text-gray-500 mb-2 text-right m-0 leading-none"><i>OauthDemo</i></p>
 
 <div><Button :state="functionState('google')"  :click="() => clicked('google') ">Continue with Google</Button></div>
